90240e8cc6173c8b842aa2768fef46a898e44e0a
Thimira Sadeesha <thimira@maximimpressions.com>
2025-11-21 20:26:45 +0530

    updated

--- repliq-db.rb
+++ repliq-db.rb
@@ -1,6 +1,6 @@
 class RepliqDb < Formula
   desc "MySQL database backup and copy CLI tool"
-  homepage "https://github.com/ThimiraSadeesha/repliq-db-cli"
+  homepage "https://github.com/ThimiraSadeesha/db-backup-cli"
   url "https://registry.npmjs.org/repliq-db/-/repliq-db-1.0.2.tgz"
   sha256 "49ec8a7879523f75e65e0ace03b6ddb6a0f89b0a6bb1807a4ff66441f6e51f5c"
   license "MIT"
@@ -8,7 +8,10 @@ class RepliqDb < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *std_npm_install_args
-    bin.install "dist/cli.js" => "repliq-db"
+    # Install the npm package into libexec
+    system "npm", "install", "--global", "--prefix=#{libexec}", cached_download
+
+    # Symlink the CLI executable to bin
+    bin.install_symlink Dir["#{libexec}/bin/repliq-db"]
   end
 end
