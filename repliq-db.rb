8b91992ddd44bb67aa25b1b6410c143f96cc5472
Thimira Sadeesha <thimira@maximimpressions.com>
2025-11-22 08:45:30 +0530

    updated url

--- repliq-db.rb
+++ repliq-db.rb
@@ -1,6 +1,6 @@
 class RepliqDb < Formula
   desc "MySQL database backup and copy CLI tool"
-  homepage "https://github.com/ThimiraSadeesha/db-backup-cli"
+  homepage "https://github.com/ThimiraSadeesha/repliq-db-cli"
   url "https://registry.npmjs.org/repliq-db/-/repliq-db-1.0.3.tgz"
   sha256 "1b9f10e4effee036986ee10182998ff321cf9fed57374adf3ffc6074b83661cd"
   license "MIT"
